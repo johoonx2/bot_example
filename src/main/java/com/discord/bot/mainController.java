1621b5d8efef845687a70bfb96e9561d51da652c
hoonjo <hoonjo@linecorp.com>
2018-10-23 19:13:25 +0900

    [hoonjo] first commit

---
+++ src/main/java/com/discord/bot/mainController.java
@@ -0,0 +1,28 @@
+package com.discord.bot;
+
+import com.google.common.util.concurrent.FutureCallback;
+import de.btobastian.javacord.DiscordAPI;
+import de.btobastian.javacord.Javacord;
+import de.btobastian.javacord.entities.message.Message;
+import de.btobastian.javacord.listener.message.MessageCreateListener;
+
+public class mainController {
+    static public void main(String args[]){
+        DiscordAPI api = Javacord.getApi("NTA0MjI4ODM4NjU0NjA3Mzcx.DrCBGA.ezxMab1jKY8GH3M3HA6jrxNSwWw", true);
+        api.connect(new FutureCallback<DiscordAPI>() {
+            public void onSuccess(final DiscordAPI api) {
+                api.registerListener(new MessageCreateListener() {
+                    public void onMessageCreate(DiscordAPI api, Message message) {
+                        if(message.getContent().equals("ping")){
+                            message.reply("pong!");
+                        }
+                    }
+                });
+            }
+
+            public void onFailure(Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+}
